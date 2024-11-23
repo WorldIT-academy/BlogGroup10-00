d4c4deac02b8272ca2620cbbb14cf19a46de2cb2
Dima <you@example.com>
2024-11-23 10:45:16 +0100

    Create models

--- Blog/post_app/views.py
+++ Blog/post_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
 def view_post(request):
     return render(request, 'post_app/view_post.html')
 
 def view_all_posts(request):
-    return render(request, 'post_app/all_posts.html')
+    
+    all_posts = Post.objects.all()
+
+    return render(
+        request, 
+        'post_app/all_posts.html', 
+        context={
+            "all_posts": all_posts
+        }
+    )
